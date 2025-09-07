190259ddca71b6b5dc74f24a5c9e310958abe692
nmax <nmax@debian>
2025-09-07 16:06:29 +0200

    rng system, syntax errors fixed and unix libraries

--- bin/main.ml
+++ bin/main.ml
@@ -1,20 +1,31 @@
-let secret_num = 42 (* i'll to change to a random method later when i figure how it actually works *)
+(* 2 hours later i've barelly figured out this random library and ocaml seperators work *)
 
-let rec guess () = (* recursive function named guess *)
-  print_endline "guess the number between 1 and 100:";
+Random.self_init () (* rng init *)
+let secret_num = (Random.int 100) + 1 (* 0..99 → 1..100 *)
+
+(* debug line *)
+(* let () = Printf.printf "secret num: %d\n" secret_num *)
+
+let rec guess () =
+  print_string "guess: ";
   let input = read_line () in
   try
     let user_guess = int_of_string input in
-    if user_guess < 1 || user_guess > 100 then
-      (print_endline "cant u read, between 1 and 100"; guess()) (* call back the function *)
-    else if user_guess = secret_num then
-      print_endline "wow, u guessed the correct number"
-    else if user_guess > secret_num then
-      (print_endline "too high"; guess())
-    else
-      (print_endline "too low"; guess())
+    if user_guess < 1 || user_guess > 100 then begin
+      print_endline "look at the range";
+      guess () end
+    else if user_guess = secret_num then begin
+      print_endline "well played" end
+    else if user_guess < secret_num then begin
+      print_endline "higher";
+      guess () end
+    else begin
+      print_endline "lower";
+      guess () end
   with
-  | Failure _ -> (* dont need to collect the error*)
-     print_endline "a valid num please"; guess()
+  | Failure _ ->
+     print_endline "enter a valid int please";
+     guess ()
 
-let () = guess () (* first call the function *)
+let () = guess ()
+
